d9fc913d2d8691d73748a8ba66ddb664e74f8e2f
rogersnif <rogerbanze@gmail.com>
2022-04-29 14:55:59 +0200

    first commit

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin "_git repository link here_"
+git remote add origin "https://github.com/rogersnif/artmarcket.git"
 git push -u origin main
